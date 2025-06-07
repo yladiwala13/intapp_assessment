bf2a1ed32b1a8f6cc35412cbd3bb92348f4495ad
yahyaladiwala-jb <yahya.ladiwala@jetblue.com>
2025-06-06 21:31:08 -0400

    added in comments in build_companies and build_deals

--- build_deals.py
+++ build_deals.py
@@ -8,8 +8,10 @@ from cleaner import BSPCleaner, CRHPCleaner
 bsp_df = BSPCleaner().clean_and_transform()
 crhp_df = CRHPCleaner().clean_and_transform()
 
+# Concatenate the two dataframes
 combined_pipelines_df = pd.concat([bsp_df, crhp_df], ignore_index=True)
 
+# Select relevant columns for Deals table
 deals_df = combined_pipelines_df[
     [
         "Pipeline",
