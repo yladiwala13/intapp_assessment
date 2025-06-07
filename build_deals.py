f195ef82125a52243f0e6df15c0f7c1ffa1e1c10
yahyaladiwala-jb <yahya.ladiwala@jetblue.com>
2025-06-06 20:53:19 -0400

    refactors code to build deals table

---
+++ build_deals.py
@@ -0,0 +1,39 @@
+"""
+Script to create Deals table
+"""
+
+import pandas as pd
+from cleaner import BSPCleaner, CRHPCleaner
+
+bsp_df = BSPCleaner().clean_and_transform()
+crhp_df = CRHPCleaner().clean_and_transform()
+
+combined_pipelines_df = pd.concat([bsp_df, crhp_df], ignore_index=True)
+
+deals_df = combined_pipelines_df[
+    [
+        'Pipeline',
+        'Company Name',
+        'Current Owner',
+        'Business Description',
+        'Vertical',
+        'Sub Vertical',
+        'Enterprise Value',
+        'Equity Investment Est.',
+        'Project Name',
+        'Date Added',
+        'Sourcing',
+        'Status',
+        'Portfolio Company Status',
+        'Active Stage',
+        'Passed Rationale',
+        'Invest. Bank',
+        'Banker',
+        'Banker Email',
+        'Banker Phone Number',
+        'Transaction Type',
+        'Lead MD',
+    ]
+]
+
+print(deals_df)
